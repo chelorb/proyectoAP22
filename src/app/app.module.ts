cabd51949e1070750b69a4ef3f8f71d301c00a59
chelorb <chelorb@hotmail.com>
2022-06-21 20:22:59 -0300

    redes sociales logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { LogoAPComponent } from './Component/logo-ap/logo-ap.component';
+import { RedesSocialesComponent } from './component/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedesSocialesComponent
   ],
   imports: [
     BrowserModule,
