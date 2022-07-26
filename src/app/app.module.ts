ab5023438deeb90068bb059d4d6e4d2c8b1767fc
chelorb <chelorb@hotmail.com>
2022-07-26 20:00:50 -0300

    ver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { LogoAPComponent } from './Component/logo-ap/logo-ap.component';
-import { RedesSocialesComponent } from './component/redes-sociales/redes-sociales.component';
 import { BannerSupComponent } from './Component/banner-sup/banner-sup.component';
 import { AcercaDeComponent } from './Component/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './Component/experiencia/experiencia.component';
+import { RedesSocialesComponent } from './Component/redes-sociales/redes-sociales.component';
+import { EducacionComponent } from './Component/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './Component/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,19 @@ import { ExperienciaComponent } from './Component/experiencia/experiencia.compon
     RedesSocialesComponent,
     BannerSupComponent,
     AcercaDeComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+  
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgCircleProgressModule.forRoot({}),
+    AppRoutingModule,
+        
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
