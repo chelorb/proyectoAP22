d8c4d367dbefd73626820e87d30e2fb3134a9a7f
chelorb <chelorb@hotmail.com>
2022-07-28 17:19:17 -0300

    iniciar sesion

--- src/app/Component/header/header.component.ts
+++ src/app/Component/header/header.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/Modelos/usuario';
+import { HeaderService } from 'src/app/Servicios/header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public usuario: Usuario|undefined;
+  public editUsuario: Usuario|undefined;
+
+  constructor(private headerService:HeaderService) { }
 
   ngOnInit(): void {
+
+    this.getUser();
+  }
+
+  public getUser():void{
+
+    this.headerService.getUser().subscribe({
+
+      next: (response: Usuario) => {
+
+        this.usuario=response;
+
+      },
+
+      error:(error:HttpErrorResponse)=>{
+
+        alert(error.message);
+      }
+      })
   }
 
 }
--- src/app/Servicios/header.service.ts
+++ src/app/Servicios/header.service.ts
@@ -1,6 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Usuario } from '../Modelos/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,19 @@ export class HeaderService {
 
 
 
-  constructor(private http: HttpClientModule) { 
+  constructor (private http: HttpClient) {   }
 
+  public getUser():Observable<Usuario> {
+
+    return this.http.get<Usuario>(`{$this.apiServerUrl}/usuario/id/1`);
   }
+
+public updateUsuario(usuario: Usuario):Observable<Usuario>{
+
+  return this.http.put<Usuario>(`${this.apiServerUrl}usuario/update`, usuario);
+
+
+}
+
 }
 
