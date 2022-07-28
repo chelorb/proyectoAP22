e91a01ea50047e65cb63c04825d8d88041869ce8
chelorb <chelorb@hotmail.com>
2022-07-28 17:33:29 -0300

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciarSesionComponent } from './Component/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './Component/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: 'portfolio', component:PortfolioComponent},
+{path: 'iniciar-secion', component:IniciarSesionComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
